41d1a8d751b3f5ed6a4e436180e0f3f21923da56
K1pore <108589318+K1pore@users.noreply.github.com>
2022-07-17 17:05:00 +0500

    урок https://www.youtube.com/watch?v=wV8fIYv1wdc&list=PLRJuPW6BGThue5qX6AkARnqc8o_80fbQA&index=14

--- StoredProperties.ComputedProperties.LazyProperties.playground/Contents.swift
+++ StoredProperties.ComputedProperties.LazyProperties.playground/Contents.swift
@@ -1,6 +1,14 @@
 import UIKit
+// lesson Урок Swift - 14 - Свойства. Stored Properties, Computed Properties, Lazy Properties. https://www.youtube.com/watch?v=wV8fIYv1wdc&list=PLRJuPW6BGThue5qX6AkARnqc8o_80fbQA&index=14
 
 struct Car {
+    static var count:Int = 0
+    // в классах метода вместо static надо писать слово class
+    // по скольку дочерние классы не смогут переопределять этот метод
+    static func getNumberCars() -> Int {
+        return Car.count
+    }
+    
     var model: String
     var mark: String
     var year: Int{
@@ -35,7 +43,13 @@ struct Car {
             }
         }
     }
-    var price: Int = getPrice()
+lazy var price: Int = getPrice()
+    init(mark:String, model: String, year: Int){
+        self.mark = mark
+        self.model = model
+        self.year = year
+        Car.count += 1
+    }
 }
 func getPrice() -> Int{
     print("Start")
@@ -48,12 +62,16 @@ func getPrice() -> Int{
     return 30000
 }
 
-var car = Car(model: "RAW4", mark: "Toyota", year: 2020)
-
+var car = Car(mark: "Toyota", model: "RAW4", year: 2020)
+var car2 = Car(mark: "Toyota", model: "Corolla", year: 2020)
+var car3 = Car(mark: "Toyota", model: "MARKII", year: 2020)
 print(car.fullName)
+print(car.price)
+print(Car.getNumberCars())
 // willSet, didSet. Работают только когда есть изминение в экземпляре
 //car.year = 1999
 //print(car.fullName)
 //car.fullName = "Toyota Corolla 2010"
 //print(car.fullName)
+
 
